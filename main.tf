029461489c64ea20d45e523d22c6ceb8a1d7440e
sachinlaturkar <107913977+sachinlaturkar@users.noreply.github.com>
2023-12-04 23:32:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-centralindia"
-    storage_account_name = "terraformgithubactions23"
+    storage_account_name = "tfstatazresearch"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     #use_oidc             = true
